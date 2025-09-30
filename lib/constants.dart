c2f844cbb6bf004e0ee6dbf74fd233b3c87829e0
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-09-30 15:40:58 +0900

    Pet Profile Page using Table

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const profileTextStyle = TextStyle(
+    color: Color(0xFF2B323A),
+    fontWeight: FontWeight.bold
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'constants.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Timo',
+      home: ProfilePage(),
+    );
   }
 }
 
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Timo',
+          style: TextStyle(
+            color: Color(0xFF2B323A),
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 30.0),
+        child: Column(
+          children: <Widget>[
+            //ProfileImage
+            Center(
+              child: Text('Timo', style: profileTextStyle,),
+            ), //pet's name
+            Center(
+              child: Text('@username', style: profileTextStyle,),
+            ),
+            SizedBox(height: 40.0,),
+            Table(
+              children: <TableRow>[
+                TableRow(
+                  children: <Widget>[
+                    TableCell(child: Text('お誕生日', style: profileTextStyle,)),
+                    TableCell(child: Text('2021/06/24', style: profileTextStyle,)),
+                  ],
+                ),
+                TableRow(
+                  children: <Widget>[
+                    TableCell(child: Text('犬種', style: profileTextStyle,)),
+                    TableCell(child: Text('柴犬', style: profileTextStyle,)),
+                  ],
+                ),
+                TableRow(
+                  children: <Widget>[
+                    TableCell(child: Text('好きなこと', style: profileTextStyle,)),
+                    TableCell(child: Text('おやつ・家族', style: profileTextStyle,)),
+                  ],
+                ),
+                TableRow(
+                  children: <Widget>[
+                    TableCell(child: Text('嫌いなこと', style: profileTextStyle,)),
+                    TableCell(child: Text('お風呂', style: profileTextStyle,)),
+                  ],
+                ),
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                      child: Text('去勢・避妊', style: profileTextStyle,)
+                    ),
+                    TableCell(child: Text('済', style: profileTextStyle,)),
+                  ],
+                ),
+                TableRow(
+                  children: <Widget>[
+                    TableCell(
+                        child: Text('飼い主さんからコメント', style: profileTextStyle,)
+                    ),
+                    TableCell(
+                        child: Text('')
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
