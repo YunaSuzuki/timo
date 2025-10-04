65343546689e9ba9d009dd090af28f19dab3a800
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-10-04 12:39:25 +0900

    Refactoring BottomNavigationBar
    ・made components directory
    ・refactoring BottomNavigationBar
    ・slight modification

--- lib/screen/pet_list_page.dart
+++ lib/screen/pet_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:timo/constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:timo/components/bottomNavigationBar.dart';
 
 class PetListPage extends StatelessWidget {
 
@@ -343,16 +344,7 @@ class PetListPage extends StatelessWidget {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.grey[900],), label: 'Home', ),
-          BottomNavigationBarItem(icon: Icon(Icons.search, color: Colors.grey[900],), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.add, color: Colors.grey[900],), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.schedule, color: Colors.grey[900],), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble, color: Colors.grey[900],), label: 'message'),
-        ]
-      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
     );
   }
 }
