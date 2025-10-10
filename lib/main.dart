9a379add50c4aad3065ab8a0b1921f8b14a66337
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-10-10 13:40:09 +0900

    slight modification to pet_list_page
    ListView  =>  ListView.separated

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Timo',
-      home: ChatList(),
+      home: PetListPage(),
       // initialRoute: '/',
       // routes: <String, WidgetBuilder>{
       //   '/': (BuildContext context) => PetListPage()
