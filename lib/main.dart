7511469aa4732585043f284a74bdafdb1eed62d6
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-10-10 13:29:23 +0900

    add chat_list_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timo/screen/chat_list.dart';
 import 'package:timo/screen/login_page.dart';
 import 'package:timo/screen/pet_list_page.dart';
 import 'screen/pet_profile_page.dart';
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Timo',
-      home: LoginPage(),
+      home: ChatList(),
       // initialRoute: '/',
       // routes: <String, WidgetBuilder>{
       //   '/': (BuildContext context) => PetListPage()
