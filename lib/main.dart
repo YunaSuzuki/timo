4db921513749e34157deba2a8a3eaf4d2c8ebea3
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-10-21 14:32:35 +0900

    add signup func
    add signup function with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timo/authgate.dart';
 import 'package:timo/screen/chat_list.dart';
 import 'package:timo/screen/login_page.dart';
 import 'package:timo/screen/main_page.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
       title: 'Timo',
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => MainPage(),
+        '/': (BuildContext context) => AuthGate(),
         'chat_list': (BuildContext context) => ChatList(),
         'pet_profile_page' : (BuildContext context) => ProfilePage(),
         'chat': (BuildContext context) => Chat(),
---
+++ lib/screen/signup_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+
+  Future<void> signUp() async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('登録が完了しました！')),
+      );
+    } on FirebaseAuthException catch (e) {
+      String message = '';
+      if (e.code == 'weak-password') {
+        message = 'パスワードが弱すぎます。';
+      } else if (e.code == 'email-already-in-use') {
+        message = 'このメールはすでに使用されています。';
+      } else if (e.code == 'invalid-email') {
+        message = 'メールアドレスが正しくありません。';
+      } else {
+        message = '登録に失敗しました: ${e.message}';
+      }
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('ユーザー登録')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'メールアドレス'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'パスワード'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: signUp,
+              child: const Text('登録'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
