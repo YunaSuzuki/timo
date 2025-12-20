45d7df8cebf0e6e19fb435fd5c19f4f81578ab85
YunaSuzuki <yuna.com.jp@outlook.jp>
2025-12-20 12:03:51 +0900

    Talk UserListPage LayoutGrid

--- lib/screen/user_list_page.dart
+++ lib/screen/user_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'chat_page.dart';
 
 class UserListPage extends StatelessWidget {
@@ -11,7 +12,7 @@ class UserListPage extends StatelessWidget {
     final currentUser = FirebaseAuth.instance.currentUser!;
 
     return Scaffold(
-      appBar: AppBar(title: const Text('ともだち')),
+      appBar: AppBar(title: const Text('Talk', style: TextStyle(fontFamily: 'Quicksand', fontWeight: FontWeight.w700, letterSpacing: 1.0),)),
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance.collection('users').snapshots(),
         builder: (context, snapshot) {
@@ -24,7 +25,26 @@ class UserListPage extends StatelessWidget {
               final user = users[index];
               final data = user.data() as Map<String, dynamic>;
               return ListTile(
-                title: Text(data['username'] ?? 'ユーザーネームなし'),
+                title: LayoutGrid(
+                  columnSizes: [50.px, 12.px, 1.fr],
+                  rowSizes: [50.px],
+                  children: [
+                    Align(
+                      child: CircleAvatar(
+                        radius: 56,
+                        backgroundImage: AssetImage('assets/images/header_image.jpg'),
+                        backgroundColor: Colors.grey[200],
+                      )
+                    ).withGridPlacement(columnStart: 0, rowStart: 0),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        data['username'] ?? 'no username',
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ).withGridPlacement(columnStart: 2, rowStart: 0),
+                  ],
+                ),
                 onTap: () {
                   Navigator.push(
                     context,
