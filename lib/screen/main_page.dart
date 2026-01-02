b6a10eac6d9211d0c324c3c57f5d38ee980856d6
YunaSuzuki <yuna.com.jp@outlook.jp>
2026-01-02 21:19:04 +0900

    BNB Home tapped -> UserProfilePage
    Bottom Navigation Bar Home tapped -> return to currentUser Profile page anytime

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timo/constants.dart';
 import 'package:timo/screen/pet_list_page.dart';
 import 'package:timo/screen/user_list_page.dart';
 import 'package:timo/screen/userHomeProfile.dart';
@@ -27,7 +28,7 @@ class _MainPageState extends State<MainPage> {
     _targetUserId = currentUserId;
   }
 
-  void _openPetList(String userId) {
+  void _openProfile(String userId) {
     setState(() {
       _targetUserId = userId;
       _selectedIndex = 0; // Homeタブに切り替え
@@ -37,33 +38,34 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     final pages = [
-      UserHomeProfile(userId: _targetUserId!),
+      UserHomeProfile(key:ValueKey(_targetUserId), userId: _targetUserId!),
       PetListPage(userId: currentUserId),
       PetListPage(userId: currentUserId),
       PetListPage(userId: currentUserId),
-      UserListPage(onAvatarTap: _openPetList), // ← コールバックで切替
+      UserListPage(onAvatarTap: _openProfile), // ← コールバックで切替
     ];
 
     return Scaffold(
       body: pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
+        selectedItemColor: greyDark,
+        unselectedItemColor: greyDark,
         onTap: (i) {
           setState(() {
+            _selectedIndex = i;
             if (i == 0) {
-              // ← Home を押したら自分のプロフィールへ戻す
               _targetUserId = currentUserId;
             }
-            _selectedIndex = i;
           });
         },
         type: BottomNavigationBarType.fixed,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: 'Home', ),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: 'add'),
           BottomNavigationBarItem(icon: Icon(Icons.schedule), label: 'timeline'),
-          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble), label: 'message'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat_bubble_outline), label: 'message'),
         ],
       ),
     );
